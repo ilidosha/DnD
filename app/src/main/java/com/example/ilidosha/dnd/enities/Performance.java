295669571f707bbf2c831c016af8b381713f814a
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-11-25 01:47:45 +0500

    + подстаты

---
+++ app/src/main/java/com/example/ilidosha/dnd/enities/Performance.java
@@ -0,0 +1,32 @@
+package com.example.ilidosha.dnd.enities;
+
+public enum Performance {
+    ATHLETICS("Атлетика"),
+    ACROBATICS("Акробатика"),
+    SLEIGHT_OF_HAND("Ловкость рук"),
+    STEALTH("Скрытность"),
+    ARCANA("Магия"),
+    HISTORY("История"),
+    INVESTIGATION("Анализ"),
+    NATURE("Природа"),
+    RELIGION("Религия"),
+    ANIMAL_HANDLING("Уход за животными"),
+    INSIGHT("Проницательность"),
+    MEDICINE("Медицина"),
+    PERCEPTION("Восприятие"),
+    SURVIVAL("Выживание"),
+    DECEPTION("Обман"),
+    INTIMIDATION("Запугивание"),
+    PERFORMANCE("Выступление"),
+    PERSUASION("Убеждение");
+
+    private String name;
+
+    Performance(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
